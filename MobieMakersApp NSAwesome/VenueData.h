7572fcdfb1b8119e44bafee4a153eced71e81c9f
RHINO <RHINO@RHINO-MacBook-Pro.local>
2013-03-06 10:36:02 -0600

    RHINO Venue class

---
+++ MobieMakersApp NSAwesome/VenueData.h
@@ -0,0 +1,13 @@
+//
+//  VenueData.h
+//  MobieMakersApp NSAwesome
+//
+//  Created by RHINO on 3/6/13.
+//  Copyright (c) 2013 Ian Blue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VenueData : NSObject
+
+@end
---
+++ MobieMakersApp NSAwesome/VenueData.m
@@ -0,0 +1,13 @@
+//
+//  VenueData.m
+//  MobieMakersApp NSAwesome
+//
+//  Created by RHINO on 3/6/13.
+//  Copyright (c) 2013 Ian Blue. All rights reserved.
+//
+
+#import "VenueData.h"
+
+@implementation VenueData
+
+@end
